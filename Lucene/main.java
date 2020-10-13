f5a9882d036a25b055e2acba1e63503fdb207d03
senne <senne.rosaer@student.uantwerpen.be>
2020-10-13 14:53:44 +0200

    first

---
+++ Lucene/main.java
@@ -0,0 +1,10 @@
+import org.apache.lucene.analysis.*;
+import org.apache.lucene.analysis.standard.*;
+
+
+class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        Analyzer analyzer = new StandardAnalyzer();
+    }
+}
