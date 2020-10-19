fd736d017bb2938b1e3c6d05b6f4c7b20d62b126
senne <senne.rosaer@student.uantwerpen.be>
2020-10-19 15:22:09 +0200

    splitting script

--- Lucene/XML_splitter.py
+++ Lucene/XML_splitter.py
@@ -1,26 +1,36 @@
-import xml.etree.ElementTree as ET
+import xml.etree.cElementTree as ET
 import os.path
 import json
+import xml.sax
+
 
 if __name__ == '__main__':
 
-    tree = ET.parse('example.xml')
-    root = tree.getroot()
-    for child in root:
-        if child.attrib['PostTypeId'] == '1' :
-            if not os.path.isfile('SplittedXML/PageID'+child.attrib['Id']):
-                f = open("SplittedXML/PageID"+child.attrib['Id'], "w+")
-                f.write(json.dumps(child.attrib))
-                f.write('\n')
-                f.close()
+    count = 0
+    count2 = 0
+    testdict = dict()
+    for event, elem in ET.iterparse("/home/senne/School/Master_AI_1/Posts.xml", events=("start", "end")):
+        if event == "end":
+            if count2 % 10000 == 0:
+                print(count2)
+            if elem.attrib['PostTypeId'] == '1' and count < 1000:
+                if not os.path.isfile('SplittedXML/PageID' + elem.attrib['Id']):
+                    f = open("SplittedXML/PageID" + elem.attrib['Id'], "w+")
+                    f.write(json.dumps(elem.attrib))
+                    f.write('\n')
+                    f.close()
+                count += 1
 
-        elif child.attrib['PostTypeId']== '2':
-            if os.path.isfile('SplittedXML/PageID'+child.attrib['ParentId']):
-                f = open('SplittedXML/PageID'+child.attrib['ParentId'], "a+")
-                f.write(json.dumps(child.attrib))
-                f.write('\n')
-        else:
-            print("Help")
+
+            elif elem.attrib['PostTypeId'] == '2':
+                if os.path.isfile('SplittedXML/PageID' + elem.attrib['ParentId']):
+                    f = open('SplittedXML/PageID' + elem.attrib['ParentId'], "a+")
+                    f.write(json.dumps(elem.attrib))
+                    f.write('\n')
+            else:
+                pass
+
+            count2 += 1
 
     '''
     f = open('SplittedXML/PageID4')
